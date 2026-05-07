c58a537622882405194fcb4a0b40947fb9b527f9
yeabsira12 <yabekbekele@gmail.com>
2026-05-07 21:43:29 +0300

    Connect to Firebase project c-n-s-f7175

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,11 +1,19 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged, User } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from '../firebase-applet-config.json';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBpxMNPbyH-_DrCtg8VZC2WfBtdo85o9D0",
+  authDomain: "c-n-s-f7175.firebaseapp.com",
+  projectId: "c-n-s-f7175",
+  storageBucket: "c-n-s-f7175.firebasestorage.app",
+  messagingSenderId: "724796544045",
+  appId: "1:724796544045:web:f3fbc7c7c2b5eefff754a4"
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app, (firebaseConfig as any).firestoreDatabaseId);
+export const db = getFirestore(app);
 
 export const googleProvider = new GoogleAuthProvider();
 
